e0cf639452bfa9c3893ae6475fc1a7e41ef04712
unknown <ew562c@A4799164.mw.nos.boeing.com>
2013-07-15 12:22:23 -0500

    Initial Logic for Scanning

--- png_print.py
+++ png_print.py
@@ -1,8 +1,19 @@
 from PIL import Image
 from optparse import OptionParser
+import sys
+
+#import and init pygame
+import pygame
+from pygame import gfxdraw
+
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="svgfile", help="SVG Layer File to Print")
+parser.add_option("-v", "--vertical", action="store_true", dest="vertical", default=False,
+                  help="Scan vertically")
+parser.add_option("-p", "--preview", action="store_true", dest="preview", default=False,
+                  help="Preview Scan")
+
 
 (options, args) = parser.parse_args()
 
@@ -10,16 +21,31 @@ i = Image.open(options.svgfile)
 
 pixels = i.load() # this is not a list
 width, height = i.size
-row_averages = []
-for y in range(height):
-    cur_row_ttl = 0
-    for x in range(width):
-        cur_pixel = pixels[x, y]
-        cur_pixel_mono = sum(cur_pixel) / len(cur_pixel)
-        cur_row_ttl += cur_pixel_mono
 
-    cur_row_avg = cur_row_ttl / width
-    row_averages.append(cur_row_avg)
+if options.preview:
+    pygame.init() 
+    #create the screen
+    window = pygame.display.set_mode((width, height)) 
+
+cur_pixel = pixels[0, 0]
+print cur_pixel
+
+if options.vertical:
+    for x in range(width):
+        for y in range(height):
+            cur_pixel = pixels[x, y]
+            if cur_pixel[0] != 255:
+                if options.preview:
+                    pygame.draw.line(window, (255, 255, 255), (x, y), (x, y))
+                    pygame.display.flip() 
+else:
+    for y in range(height):
+        for x in range(width):
+            cur_pixel = pixels[x, y]
+            if cur_pixel[0] != 255:
+                if options.preview:
+                    pygame.draw.line(window, (255, 255, 255), (x, y), (x, y))
+                    pygame.display.flip() 
 
 print options.svgfile,
 print " Height:",
